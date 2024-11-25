3d104843fa076248a4e4f61489d4dc4d8b027f75
Alicev-itoria <Avss13@aluno.ifal.edu.br>
2024-11-26 05:03:17 +1300

    Add files via upload

---
+++ atv2.js
@@ -0,0 +1,7 @@
+let valorDolar = parseFloat(prompt("Digite o valor em dólares:"));
+
+let cotacaoDolar = 5.30;
+
+let valorReal = valorDolar * cotacaoDolar;
+
+alert("O valor de " + valorDolar + " dólares é equivalente a " + valorReal+ " reais.");
